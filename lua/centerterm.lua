fe510badc91110ed21cb4ae3d74aeb117b8a361c
alteredoxide <me@alteredoxide.com>
2023-09-16 14:09:24 -0700

    add to setup
    Added various items to setup that were in my own init, but not contained
    within the plugin itself. The following additions were made:
    - Set the following vim commands:
      - Center -- toggle center
      - Vs -- create new split and toggle center
      - Vx -- close current split and re-center
    - optional default keybindings for vim commands
    
    Also removed some commented code and performed minor formatting.

--- lua/centerterm.lua
+++ lua/centerterm.lua
@@ -1,4 +1,6 @@
 local M = {}
+M.center_width = 120
+
 
 local function create_blank_buffer(width)
   vim.cmd("vnew")
@@ -11,6 +13,7 @@ local function create_blank_buffer(width)
   vim.opt.fillchars:append("vert: ")
   vim.opt.fillchars:append({ eob = ' ' })
 end
+
 
 local function create_centered_buffer(width)
   local total_width = vim.o.columns
@@ -29,6 +32,7 @@ local function create_centered_buffer(width)
   return true
 end
 
+
 local centered = false
 
 function M.toggle_centered_buffer(width)
@@ -40,37 +44,73 @@ function M.toggle_centered_buffer(width)
   end
 end
 
+
+function M.quit_vertical_split_and_toggle()
+    vim.cmd("q")
+    M.toggle_centered_buffer(M.center_width)
+end
+
+
 function M.vertical_split_and_toggle(width)
     M.toggle_centered_buffer(width)
     vim.cmd("vs")
 end
 
---local function setup_autocmd()
---    vim.cmd([[
---        augroup DetectVimResize
---        autocmd! * <buffer>
---    ]]..
---    "autocmd VimResized * lua require('centerterm')"..
---    ".toggle_centered_buffer(vim.g.centerterm_width)"..
---    [[
---        augroup END
---    ]]
---)
---end
+
+function M.set_default_keybindings()
+    local bind_opts = { noremap=true, silent=true }
+    -- Toggle center
+    vim.keymap.set("n", "<leader>cc",  "<cmd>Center<CR>", bind_opts)
+    -- Vertical split with toggle center
+    vim.keymap.set("n", "<leader>vs",  "<cmd>Vs<CR>", bind_opts)
+    -- Close current split then toggle center
+    vim.keymap.set("n", "<leader>vx", "<cmd>Vx<CR>", bind_opts)
+end
+
 
 local function setup_autocmd()
-  vim.cmd([[
-    augroup DetectVimResize
-      autocmd! * <buffer>
-      autocmd VimResized * lua require('centerterm').toggle_centered_buffer(vim.g.centerterm_width)
-    augroup END
-  ]])
+    vim.cmd([[
+        augroup DetectVimResize
+            autocmd! * <buffer>
+            autocmd VimResized * Center
+        augroup END
+    ]])
 end
+
+
+local function set_vim_commands()
+    vim.cmd(
+    "command! Center lua require('centerterm')"..
+    ".toggle_centered_buffer(vim.g.centerterm_width)"
+    )
+    -- Create new vertical split and toggle Center
+    vim.cmd(
+    "command! Vs lua require('centerterm')"..
+    ".vertical_split_and_toggle(vim.g.centerterm_width)"
+    )
+    -- Quit current vertical split and toggle Center
+    vim.cmd(
+    "command! Vx lua require('centerterm')"..
+    ".quit_vertical_split_and_toggle()"
+    )
+end
+
 
 -- Initialize autocmds
-function M.setup()
+function M.setup(opts)
+    opts = opts or {}
+    if opts.default_keybindings then
+        M.set_default_keybindings()
+    end
+    if opts.center_width then
+        M.center_width = opts.center_width
+    end
+    vim.g.centerterm_width = M.center_width
+    -- Define the command to toggle the centered buffer
+    set_vim_commands()
     setup_autocmd()
-end
+    -- Enable the centered buffer by default
+    vim.cmd("autocmd VimEnter * Center")
+    end
 
 return M
-
